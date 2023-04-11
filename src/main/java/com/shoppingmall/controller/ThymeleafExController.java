ff10130454a60454ce5a0c904da6890fcd911f95
James Bond <hooney200@gmail.com>
2023-04-12 01:30:39 +0900

    Spring Boot 쇼핑몰 프로젝트 - Thymeleaf 예제 및 활용

--- src/main/java/com/shoppingmall/controller/ThymeleafExController.java
+++ src/main/java/com/shoppingmall/controller/ThymeleafExController.java
@@ -20,6 +20,7 @@ public class ThymeleafExController {
         return "thymeleaf/thymeleafEx01";
     }
 
+    /* 타임리프 th:text 문법 예제*/
     @GetMapping(value = "/ex02")
     public String thymeleafEx02(Model model) {
         ItemDTO item = new ItemDTO();
@@ -33,6 +34,7 @@ public class ThymeleafExController {
 
     }
 
+    /* 타임리프 반복문 th:each 문법 예제*/
     @GetMapping(value = "/ex03")
     public String thymeleafEx03(Model model) {
 
@@ -51,6 +53,7 @@ public class ThymeleafExController {
         return "thymeleaf/thymeleafEx03";
     }
 
+    /* 타임리프 반복문 th:if, th:unless 문법 예제 */
     @GetMapping(value = "/ex04")
     public String thymeleafEx04(Model model) {
 
@@ -67,6 +70,45 @@ public class ThymeleafExController {
         }
         model.addAttribute("items", itemList);
         return "thymeleaf/thymeleafEx04";
+    }
+
+    /* 타임리프 반복문 th:switch, th:case 문법 예제 */
+    @GetMapping(value = "/ex04-01")
+    public String thymeleafEx04_01(Model model) {
+
+        List<ItemDTO> itemList = new ArrayList<>();
+
+        for (int i = 0; i <= 10; i++) {
+            ItemDTO item = new ItemDTO();
+            item.setDescription("test 신발" + i + "입니다.");
+            item.setName("test 신발" + i);
+            item.setPrice(40000 + i);
+            item.setCreateTime(LocalDateTime.now());
+
+            itemList.add(item);
+        }
+        model.addAttribute("items", itemList);
+        return "thymeleaf/thymeleafEx04-01";
+    }
+
+    /* 타임리프 th:href 문법 예제 */
+    @GetMapping(value = "/ex05")
+    public String thymeleafEx05() {
+        return "thymeleaf/thymeleafEx05";
+    }
+
+    /* 타임리프 param 문법을 활용 파라미터 데이터 전달 예제 */
+    @GetMapping(value = "/ex06")
+    public String thymeleafEx06(String param1, String param2, Model model) {
+        model.addAttribute("param1", param1);
+        model.addAttribute("param2", param2);
+        return "thymeleaf/thymeleafEx06";
+    }
+
+    /* 타임리프 페이지 레이아웃 활용 예제 */
+    @GetMapping(value = "/ex07")
+    public String thymeleafEx07() {
+        return "thymeleaf/thymeleafEx07";
     }
 
 
