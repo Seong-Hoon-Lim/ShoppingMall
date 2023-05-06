142ee32c5ebdc339d90252f865abef106d8390fb
James Bond <hooney200@gmail.com>
2023-05-06 14:39:07 +0900

    Spring Boot 쇼핑몰 프로젝트 - 스프링 시큐리티 설정

--- src/main/java/com/shoppingmall/config/SecurityConfig.java
+++ src/main/java/com/shoppingmall/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()
                 .loginPage("/members/login")    //로그인 페이지 URL 설정
-                .defaultSuccessUrl("/")     //로그인 성공시 이동할 URL 설정
+                .defaultSuccessUrl("/", true)     //로그인 성공시 이동할 URL 설정
                 .usernameParameter("email")     //로그인 시 사용할 파라미터 이름으로 email을 지정
                 .failureUrl("/members/login/error")    //로그인 실패시 이동할 URL을 설정
                 .and()
@@ -85,6 +85,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/css/**", "/js/**", "/images/**");
+        web.ignoring().antMatchers("/css/**", "/js/**", "/images/**", "**/error");
     }
 }
